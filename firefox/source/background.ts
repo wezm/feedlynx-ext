78bb50de1586d469f50082223ccb11ea47ba4225
Wesley Moore <wes@wezm.net>
2024-07-10 13:04:07 +1000

    Initial functional Firefox extension

--- firefox/source/background.ts
+++ firefox/source/background.ts
@@ -1,13 +1,59 @@
 "use strict";
 
-// const FEDIURL_HOST = new URL("http://127.0.0.1:8000/");
-//const FEDIURL_HOST = new URL("https://fediurl.7bit.org/");
-
 browser.browserAction.onClicked.addListener((tab) => {
-	if (!tab.url) {
-		return;
-	}
+  if (tab.url) {
+    const url = tab.url;
 
-	const url = new URL(`/${tab.url}`, FEDIURL_HOST);
-	browser.tabs.update(tab.id, {url: url.toString()});
+    browser.storage.sync.get(["host", "token"]).then((results) => {
+      // TODO: Handle empty strings
+      if (typeof results.host == "string" && typeof results.token == "string") {
+        const post_url = new URL("/add", results.host);
+
+        const body = new URLSearchParams({
+          url: url,
+          token: results.token,
+        });
+
+        if (typeof tab.title == "string" && tab.title !== "") {
+          body.set("title", tab.title);
+        }
+
+        fetch(post_url, {
+          method: "POST",
+          body: body,
+          headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        })
+          .then((response) => {
+            // TODO: Show the response body
+            if (response.status != 201) {
+              notify(
+                "Unable to add URL",
+                `The request to add the URL was unsuccessful (${response.status})`,
+              );
+            }
+            return response.text();
+          })
+          .then((text) => notify("Added", "The link was added."))
+          .catch((e) => {
+            notify("Unable to add URL", `The request to add the URL was unsuccessful.`);
+          });
+      } else {
+        notify(
+          "Feedlynx not set up",
+          "The Feedlynx extension needs to have the host and token configured.",
+        );
+      }
+    });
+  } else {
+    notify("Error", "This tab does not have a URL.");
+  }
 });
+
+function notify(title: string, message: string) {
+  browser.notifications.create(null, {
+    type: "basic",
+    iconUrl: browser.runtime.getURL("icon-96.png"),
+    title: title,
+    message: message,
+  });
+}
---
+++ firefox/source/options.ts
@@ -0,0 +1,37 @@
+function saveOptions(e: SubmitEvent) {
+  e.preventDefault();
+
+  browser.storage.sync.set({
+    host: val("host"),
+    token: val("token"),
+  });
+}
+
+function restoreOptions() {
+  function onError(error: Error) {
+    browser.notifications.create(null, {
+      type: "basic",
+      iconUrl: browser.runtime.getURL("icon-96.png"),
+      title: "Unable to load settings",
+      message: `Error: ${error}`,
+    });
+  }
+
+  let getting = browser.storage.sync.get(["host", "token"]);
+  getting.then((results) => {
+    el("host").value = results.host?.toString() || "";
+    el("token").value = results.token?.toString() || "";
+  }, onError);
+}
+
+function el(id: string) {
+  return document.getElementById(id) as HTMLInputElement;
+}
+
+function val(id: string) {
+  const element = el(id);
+  return element.value;
+}
+
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.getElementById("form")?.addEventListener("submit", saveOptions);
